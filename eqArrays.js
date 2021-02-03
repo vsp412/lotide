c9fecaa12c0ab6318f1f1197a319892ab26e84f8
Vineet Pande <vsp4991@gmail.com>
2021-02-03 07:52:19 +0000

    ltide new today

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a,b){
+   if (a.length === b.length) {
+     let c = 0;
+     for(let i = 0; i < a.length; i++) {
+       if (a[ i ] !== b[ i ]){
+        c = c + 1; 
+        break;
+       }
+     }
+     if(c > 0) {
+       return false;
+     } else {
+       return true;
+     }
+     
+   } else {
+     return false;
+   }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
