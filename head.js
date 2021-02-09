fce4d9808cc718e8db389367fdf958b388a5bfb3
Vineet Pande <vsp4991@gmail.com>
2021-02-09 04:54:53 +0000

    headtest done

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require ('./assertEqual');
 
 function head(p) {
   if(p === []) {
@@ -15,6 +9,5 @@ function head(p) {
     return p[0];
   }
 }
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
