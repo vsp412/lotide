e30368e8290796a4f236f81ec1cf3d07a6e45907
Vineet Pande <vsp4991@gmail.com>
2021-02-09 09:07:35 +0000

    final lotide

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const eqArrays = require('./eqArrays');
-const countLetters = require ('./countLetters');
-const countOnly = require ('./countOnly');
-const eqObjects = require ('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require("./flatten");
-const letterPositions = require('./letterPositions');
-const takeUntil = require ('./takeUntil');
-const without = require ('./without');
+// const eqArrays = require('./eqArrays');
+// const countLetters = require ('./countLetters');
+// const countOnly = require ('./countOnly');
+// const eqObjects = require ('./eqObjects');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
+// const flatten = require("./flatten");
+// const letterPositions = require('./letterPositions');
+// const takeUntil = require ('./takeUntil');
+// const without = require ('./without');
 
 module.exports = {
   head,
   tail,
   middle,
-  eqArrays,
-  countLetters,
-  countOnly,
-   eqObjects,
-  findKey,
-  findKeyByValue,
-  flatten,
-   letterPositions,
-   takeUntil,
-   without
+  
 };
+// eqArrays,
+//   countLetters,
+//   countOnly,
+//    eqObjects,
+//   findKey,
+//   findKeyByValue,
+//   flatten,
+//    letterPositions,
+//    takeUntil,
+//    without
