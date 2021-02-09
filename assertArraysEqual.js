64f495084c713f4ff34790ac3b6b344290843114
Vineet Pande <vsp4991@gmail.com>
2021-02-09 06:13:15 +0000

    assertarraysequal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,5 @@
-const eqArrays = function(a,b){
-  if (a.length === b.length) {
-    let c = 0;
-    for(let i = 0; i < a.length; i++) {
-      if (a[ i ] !== b[ i ]){
-       c = c + 1; 
-       break;
-      }
-    }
-    if(c > 0) {
-      return false;
-    } else {
-      return true;
-    }
-    
-  } else {
-    return false;
-  }
-}
+const eqArrays = require ('./eqArrays');
+
 
 
 const assertArraysEqual = function(actual, expected) {
@@ -27,7 +10,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, '3']);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
+module.exports = assertArraysEqual;
 
