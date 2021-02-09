4a9ec812ac3a327d19e757b93be8bbac90df6fe1
Vineet Pande <vsp4991@gmail.com>
2021-02-09 06:46:37 +0000

    m&c1

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,24 @@
 const head = require ('../head');
 const assertEqual = require ('../assertEqual');
+const { assert } = require('chai');
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("#headcheck",function () {
+
+   it("should return 5 if [5,6,7] is passed",  () => {
+        
+        assert.strictEqual(head([5, 6, 7]),5);
+   });
+
+   it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'Hello' for ['Hello', \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+
+
+});
